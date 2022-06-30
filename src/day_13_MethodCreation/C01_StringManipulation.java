a9ee1804428a97f97d2d0d8d2aa80f5241aa84e4
emresulukan <esulukan34@gmail.com>
2022-06-30 20:44:35 +0300

    29 Haziran

--- src/day_13_MethodCreation/C01_StringManipulation.java
+++ src/day_13_MethodCreation/C01_StringManipulation.java
@@ -12,13 +12,17 @@ ipucu= Double.parseDouble() metodunu kullanabilirsiniz.
             String str1 = "$13.99";
             String str2 = "$10.55";
             str1=str1.replaceAll("\\D","");
+            System.out.println("str1 = " + str1);
             double sayi1=Double.parseDouble(str1);
             str2=str2.replaceAll("\\D","");
+            System.out.println("str2 = " + str2);
             double sayi2=Double.parseDouble(str2);
             System.out.println("$"+(sayi1+sayi2)/100);
 
+            String str4 = "seyhan yavuz";
+            System.out.println(str4.replace("s", "S"));
 
-            
+            System.out.println(str4.replace("y", "Y"));
         }
 
     }
---
+++ src/day_14_/C01_.java
@@ -0,0 +1,9 @@
+package day_14_;
+
+public class C01_ {
+    public static void main(String[] args) {
+
+
+
+    }
+}
