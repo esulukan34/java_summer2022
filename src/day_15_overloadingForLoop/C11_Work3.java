8e5fce9468563fb8c083706c777acea0224c06ef
emresulukan <esulukan34@gmail.com>
2022-07-02 00:38:03 +0300

    1 Temmuz

---
+++ src/day_15_overloadingForLoop/C11_Work3.java
@@ -0,0 +1,17 @@
+package day_15_overloadingForLoop;
+
+public class C11_Work3 {
+    public static void main(String[] args) {
+
+
+        //Soru3) 100 den 50 ye kadar olan sayilari aralarinda virgul kullanarak ayni satirda yazdirin
+
+      int sayi=100;
+
+        for (int i = 100; i >=50 ; i--) {
+            System.out.print( + i+",");
+
+        }
+
+    }
+}
