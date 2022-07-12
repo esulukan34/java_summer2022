9964ea3e90c0c76d59a70c8cf1f5fa9b347f3f12
emresulukan <esulukan34@gmail.com>
2022-07-12 18:32:01 +0300

    Scope

--- src/day18_while_doWhileLoop/W12_DoWhileSoru5.java
+++ src/day18_while_doWhileLoop/W12_DoWhileSoru5.java
@@ -1,6 +1,13 @@
 package day18_while_doWhileLoop;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class W12_DoWhileSoru5 {
+    public static void main(String[] args) {
+
+
     /*
     Kullanicidan bir sifre girmesini isteyin. Girilen sifreyi asagidaki sartlara
     gore kontrol edin ve sifredeki hatalari yazdirin.
@@ -11,4 +18,12 @@ public class W12_DoWhileSoru5 {
         -Sifre ozel karakter icermelidir
         -Sifre en az 8 karakter olmalidir.
      */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lutfen bir sifre giriniz ");
+        String sifre=sc.nextLine();
+
+
+
+    }
 }
+
