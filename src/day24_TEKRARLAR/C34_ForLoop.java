269230ae9b165c6441daa11eff48ef5004b22f07
emresulukan <esulukan34@gmail.com>
2022-07-26 01:07:15 +0300

    22.07.26

--- src/day24_TEKRARLAR/C34_ForLoop.java
+++ src/day24_TEKRARLAR/C34_ForLoop.java
@@ -5,4 +5,38 @@ public class C34_ForLoop {
     // 10 dan 20 ye kadar olan sayilari toplayin
     // 30 dahil 50 dahil, aradaki cift sayilari toplayin
     // 1500 ile 1600 arasinda 7 ile bölünebilen sayilarin toplami
+    public static void main(String[] args) {
+        int toplam =0;
+
+        for (int i = 1; i <=5 ; i++) {
+            toplam += i;
+        }
+        System.out.println("toplam = " + toplam);
+
+        System.out.println("");
+        for (int i = 10; i <=20 ; i++) {
+            toplam += i;
+        }
+        System.out.println("toplam = " + toplam);
+
+        System.out.println("");
+        for (int i = 30; i <=50 ; i++) {
+            if(i%2==0){
+                toplam += i;
+            }
+
+        }
+        System.out.println("toplam = " + toplam);
+
+        System.out.println("");
+        for (int i = 1500; i <=1600 ; i++) {
+            if(i%7==0){
+                toplam += i;
+            }
+
+        }
+        System.out.println("toplam = " + toplam);
+
+
+    }
 }
--- src/day24_TEKRARLAR/C35_Faktoriyel.java
+++ src/day24_TEKRARLAR/C35_Faktoriyel.java
@@ -1,10 +1,30 @@
 package day24_TEKRARLAR;
 
 public class C35_Faktoriyel {
-     /*
-        input olarak verilen bir tamsayi için faktoriyel hesaplayip yazdiran
-        bir method olusturun.
-        Verilen sayi negatif veya 20'den buyuk olursa girilen sayinin
-        faktoriyeli hesaplanamaz uyarisi yazdirin
-         */
+    /*
+       input olarak verilen bir tamsayi için faktoriyel hesaplayip yazdiran
+       bir method olusturun.
+       Verilen sayi negatif veya 20'den buyuk olursa girilen sayinin
+       faktoriyeli hesaplanamaz uyarisi yazdirin
+        */
+    public static void main(String[] args) {
+
+        int sayi = 0;
+
+        faktoriyel(sayi);
+
+    }
+
+    public static void faktoriyel(int sayi) {
+        int faktoriyel = 1;
+
+        if (sayi >= 0 && sayi <= 20) {
+            for (int i = 1; i <= sayi; i++) {
+                faktoriyel *= i;
+            }
+            System.out.println("faktoriyel = " + faktoriyel);
+        } else {
+            System.out.println("girilen sayinin faktoriyeli hesaplanamaz");
+        }
+    }
 }
