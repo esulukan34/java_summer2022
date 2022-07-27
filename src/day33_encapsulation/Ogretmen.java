e33e90dda6ce08e068a1c7963fadf05bbe1152f3
emresulukan <esulukan34@gmail.com>
2022-07-27 23:28:59 +0300

    22.07.27

---
+++ src/day33_encapsulation/Ogretmen.java
@@ -0,0 +1,46 @@
+package day33_encapsulation;
+
+public class Ogretmen {
+       /*
+    Bazen de yetkileri sinirlandirmak degil de
+    yapilan isi daha iyi tanimlamak icin
+    encapsulation kullanilir
+
+    Bu yaklasimi kullanan class'larda
+    tum variable'lar private yapilip
+    hepsi icin getter ve setter olusturulur
+     */
+
+
+    private String isim;
+    private String soyisim;
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public String getBrans() {
+        return brans;
+    }
+
+    public void setBrans(String brans) {
+        this.brans = brans;
+    }
+
+    private String brans;
+
+
+
+}
